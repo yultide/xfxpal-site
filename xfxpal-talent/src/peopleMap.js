95293a5534c14009e74fb0fd462b14f6eacb55b2
Yulius Tjahjadi <yultide@yahoo.com>
2020-05-13 20:53:20 -0700

    Adding PeopleMap.

---
+++ xfxpal-talent/src/peopleMap.js
@@ -0,0 +1,20 @@
+export const peopleMap = {
+  'Andreas Girgensohn': 'andreas.jpg',
+  'David Ayman Shamma': 'ayman.jpg',
+  b: 'bee.png',
+  c: 'carter.jpg',
+  c2: 'chelhwon.jpg',
+  c3: 'chidansh.jpg',
+  d: 'daniel.jpg',
+  f: 'filby.jpg',
+  h: 'hao.jpg',
+  'Kandha Sankarapandian': 'kandha.jpg',
+  k: 'kimber.jpg',
+  l: 'lyndon.jpg',
+  m: 'mattlee.jpg',
+  'Mitesh Patel': 'mitesh.jpg',
+  q: 'qiong.jpg',
+  t: 'tony.jpg',
+  'Yanxia Zhang': 'yanxia.jpg',
+  y: 'yanying.jpg'
+};
