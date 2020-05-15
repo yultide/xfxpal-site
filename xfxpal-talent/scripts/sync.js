47cd11862d6b623fe22a1118cf52f1a1d009896e
Yulius Tjahjadi <yultide@yahoo.com>
2020-05-15 12:10:18 -0700

    url published csv to update prople.json

--- xfxpal-talent/scripts/sync.js
+++ xfxpal-talent/scripts/sync.js
@@ -1,23 +1,24 @@
 const csv = require('csvtojson');
 const yargs = require('yargs');
 const fs = require('fs');
-
+const request = require('request');
 
 const argv = yargs
   .scriptName("synch.js")
-  .usage('$0 [args] csvfile')
+  .usage('$0 [args]')
   .help()
   .argv;
 
-var csvFilePath = argv._[0] || './XFXPAL Talent.csv';
-var jsonFilePath = './src/people.json';
-console.log(`Processing ${csvFilePath} -> ${jsonFilePath}`);
+const dataUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vST1PSewhKjWy5inyw_loCQU_EyoVeYOX0ydmTUS-kHts-fCckIgj6fvo8ov_ZJjIskUOb7OWdOu_ix/pub?gid=415914948&single=true&output=csv'
+const jsonFilePath = './src/people.json';
+
 csv()
-  .fromFile(csvFilePath)
+  .fromStream(request.get(dataUrl))
   .then((jsonObj)=>{
       let data = JSON.stringify(jsonObj, 0, 2);
       fs.writeFileSync(jsonFilePath, data);
       console.log(`Updated ${jsonFilePath}`);
   })
-  .catch(console.error)
+  .catch(console.error);
+
 
